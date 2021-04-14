3ff28d7812b58b06ad0fde5918989b9688a57e58
Sorah Fukumori <sorah@cookpad.com>
2021-04-15 00:32:09 +0900

    support manipulating RequestedAuthnContext

--- lib/force_unspecified/app.rb
+++ lib/force_unspecified/app.rb
@@ -1,7 +1,13 @@
 require 'rack'
 require 'zlib'
 
+require 'force_unspecified/manipulations/name_id_policy'
+require 'force_unspecified/manipulations/requested_authn_context'
+require 'force_unspecified/manipulations/correct_destination'
+
 module ForceUnspecified
+  class ManipulationNameError < NameError; end
+
   class App
     def self.call(env)
       new(env).call
@@ -14,7 +20,6 @@ module ForceUnspecified
     attr_reader :request
 
     def call
-      next_hop = URI.decode_www_form_component(request.path.sub(%r{^/}, ''))
       if next_hop.empty?
         return index()
       end
@@ -22,20 +27,19 @@ module ForceUnspecified
         return [400, {'Content-Type' => 'text/plain'}, ["SAMLRequest is missing\n"]]
       end
 
-      modified_saml_request = saml_request.
-        gsub(/(['"])urn:oasis:names:tc:SAML:1.1:nameid-format:.+?(["'])/, '\1urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified\2').
-        gsub("#{request.base_url}/", '')
+      modified_saml_request = manipulations.inject(saml_request.gsub("#{request.base_url}/", '')) { |r,i| i.new(r, self).result }
       param = encode_saml_request(modified_saml_request)
 
       [302, {'Location' => "#{next_hop}?SAMLRequest=#{param}"}, ['']]
+    rescue ManipulationNameError => e
+      return [400, {'Content-Type' => 'text/plain'}, ["#{e.message}\n"]]
     end
 
     def index
       [200, {'Content-Type' => 'text/plain'}, [<<-EOF]]
 https://github.com/sorah/force_unspecified
-Modifies received SAMLRequest to force 'unspecified' as a requested NameIDPolicy, and redirects to a SAML consumer URL.
 
-Usage: #{request.base_url}/https://login.example.org/saml?SAMLRequest=xxxxx
+Usage: #{request.base_url}/manipulate/NameIDPolicy/login.example.org/saml?SAMLRequest=xxxxx
       EOF
     end
 
@@ -59,6 +63,29 @@ Usage: #{request.base_url}/https://login.example.org/saml?SAMLRequest=xxxxx
 
     def saml_request
       @saml_request ||= decode_saml_request
+    end
+
+    def next_hop
+      return @next_hop if @next_hop
+
+      next_hop = if request.path.start_with?('/manipulate/')
+                   URI.decode_www_form_component(request.path.sub(%r{^/manipulate/[^/]+/}, 'https://'))
+                 else # backward compatibility
+                   URI.decode_www_form_component(request.path.sub(%r{^/}, ''))
+                 end
+      @next_hop = next_hop.empty? ? nil : next_hop
+    end
+
+    def manipulations
+      @manipulations ||= if request.path.start_with?('/manipulate/')
+                           request.path.match(%r{^/manipulate/([^/]+?)/})&.then{ |_| _[1] } || ''
+                         else # backward compatibility
+                           'NameIDPolicy'
+                         end.split(?,).map do |name|
+                           Manipulations.const_get(name.to_sym, false)
+                         rescue NameError => e
+                           raise ManipulationNameError, e.message
+                         end.compact + [Manipulations::CorrectDestination]
     end
   end
 end
